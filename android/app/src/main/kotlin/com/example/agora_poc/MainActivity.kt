5085f4ceffbf0a3cb6b3b817b55ac4e4655fceb1
swapnilwalke <pkswalke@gmail.com>
2022-04-05 15:50:02 +0530

    feature 1

---
+++ android/app/src/main/kotlin/com/example/agora_poc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.agora_poc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:agora_rtc_engine/rtc_engine.dart';
+import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
+import 'package:agora_rtc_engine/rtc_remote_view.dart' as RtcRemoteView;
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Agora Poc'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final String APP_ID = 'b11308a064f241f292166972f0bbee9b';
+  final String Token =
+      '006b11308a064f241f292166972f0bbee9bIABlZGGOzcTDqSNMSZFM//SvgsG6dQY7fGWxiwKUjmgKSAx+f9gAAAAAEADJZeRXwEdNYgEAAQDBR01i';
+  final String CHANNEL_ID = 'test';
+  bool _joined = false;
+  int _remoteUid = 0;
+  bool _switch = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Init the app
+  Future<void> initPlatformState() async {
+    //await [Permission.camera, Permission.microphone].request();
+
+    // Create RTC client instance
+    RtcEngineContext context = RtcEngineContext(APP_ID);
+    var engine = await RtcEngine.createWithContext(context);
+    // Define event handling logic
+    engine.setEventHandler(RtcEngineEventHandler(
+        joinChannelSuccess: (String channel, int uid, int elapsed) {
+      print('joinChannelSuccess ${channel} ${uid}');
+      setState(() {
+        _joined = true;
+      });
+    }, userJoined: (int uid, int elapsed) {
+      print('userJoined ${uid}');
+      setState(() {
+        _remoteUid = uid;
+      });
+    }, userOffline: (int uid, UserOfflineReason reason) {
+      print('userOffline ${uid}');
+      setState(() {
+        _remoteUid = 0;
+      });
+    }));
+    // Enable video
+    await engine.enableVideo();
+    // Join channel with channel name as 123
+    await engine.joinChannel(Token, CHANNEL_ID, null, 0);
+  }
+
+  // Build UI
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Web Video Call 102'),
+        ),
+        body: Stack(
+          children: [
+            Center(
+              child: _switch ? _renderRemoteVideo() : _renderLocalPreview(),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _switch = !_switch;
+                    });
+                  },
+                  child: Center(
+                    child:
+                        _switch ? _renderLocalPreview() : _renderRemoteVideo(),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Local preview
+  Widget _renderLocalPreview() {
+    if (_joined) {
+      return const RtcLocalView.SurfaceView();
+    } else {
+      return const Text(
+        'Please join channel first',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+
+  Widget _renderRemoteVideo() {
+    if (_remoteUid != 0) {
+      return RtcRemoteView.SurfaceView(
+        uid: _remoteUid,
+        channelId: CHANNEL_ID,
+      );
+    } else {
+      return const Text(
+        'Please wait remote user join',
+        textAlign: TextAlign.center,
+      );
+    }
+  }
+}
